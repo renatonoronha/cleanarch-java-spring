cfab7507c22e36452ed05f9f9a9504a67217fb78
Renato Noronha <dev.renato10@gmail.com>
2024-06-14 18:05:29 -0300

    fix: remove old architecture

--- src/main/java/com/example/cleanarch_java_spring/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cleanarch_java_spring.repository;
-
-import com.example.cleanarch_java_spring.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
