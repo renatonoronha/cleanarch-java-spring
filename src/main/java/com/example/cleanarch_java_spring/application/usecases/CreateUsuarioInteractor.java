305fd9c6585e29e7ce88bb7a300ecfffd1614702
Renato Noronha <dev.renato10@gmail.com>
2024-06-14 17:59:38 -0300

    fix: alter constructor of usecase CreateUsuarioInteractor to public

--- src/main/java/com/example/cleanarch_java_spring/application/usecases/CreateUsuarioInteractor.java
+++ src/main/java/com/example/cleanarch_java_spring/application/usecases/CreateUsuarioInteractor.java
@@ -6,7 +6,7 @@ import com.example.cleanarch_java_spring.domain.entity.Usuario;
 public class CreateUsuarioInteractor {
     private final UsuarioGateway usuarioGateway;
 
-    private CreateUsuarioInteractor(UsuarioGateway usuarioGateway){
+    public CreateUsuarioInteractor(UsuarioGateway usuarioGateway){
         this.usuarioGateway = usuarioGateway;
     }
 
