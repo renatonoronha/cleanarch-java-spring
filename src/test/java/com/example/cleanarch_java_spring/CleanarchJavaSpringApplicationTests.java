20e98fcdb29e559b5158c64308859dd6ec5cd025
Renato Noronha <dev.renato10@gmail.com>
2024-06-14 14:11:03 -0300

    feat: initial config

---
+++ src/test/java/com/example/cleanarch_java_spring/CleanarchJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanarch_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
